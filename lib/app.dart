214f150a1c99c82536e6dc2cd33f17c744fc1e66
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-09-13 01:40:02 +0100

    Migrate from Getx to using Go-Router for Routing

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get/route_manager.dart';
-import 'package:up_todo/features/authenication/screens/onboarding/on_boarding.dart';
-import 'package:up_todo/utils/theme/theme.dart';
+import 'package:up_todo/src/utils/routing/route_config.dart';
+import 'package:up_todo/src/utils/theme/theme.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -13,10 +12,10 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp.router(
       themeMode: ThemeMode.dark,
       theme: UAppTheme.appTheme,
-      home: const OnBoardingScreen(),
+      routerConfig: router,
     );
   }
 }
