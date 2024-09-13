bdbd257a5dd77f5b74685a1dcb6046f7521f758a
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-09-13 01:40:42 +0100

    Init Storage, Firebase, Provider Scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:up_todo/app.dart';
+import 'package:up_todo/firebase_options.dart';
 
-void main() {
-  // TODO: Add Widget Binding
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  // -- Widget Binding
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(systemNavigationBarColor: Colors.transparent));
-  // TODO: Init Local Storage
-  // TODO: Await Native Splash
-  // TODO: Initialize Firebase
+
+  // -- Init Local Storage
+  await SharedPreferences.getInstance();
+
+  // -- Await Native Splash
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // -- Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // TODO: Initialize Authenication
 
-  runApp(const App());
+  runApp(const ProviderScope(child: App()));
 }
