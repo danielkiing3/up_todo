d44e89563036eb626e65e2e30566d581edcd0cbd
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-09-16 18:44:43 +0100

    Update to use Riverpod

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:up_todo/src/utils/routing/route_config.dart';
 import 'package:up_todo/src/utils/theme/theme.dart';
 
-class App extends StatefulWidget {
+class App extends ConsumerStatefulWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => _AppState();
+  ConsumerState<App> createState() => _AppState();
 }
 
-class _AppState extends State<App> {
+class _AppState extends ConsumerState<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       themeMode: ThemeMode.dark,
       theme: UAppTheme.appTheme,
+      localizationsDelegates: const [
+        //     GlobalMaterialLocalizations.delegate,
+        // GlobalWidgetsLocalizations.delegate,
+        // GlobalCupertinoLocalizations.delegate,
+      ],
       routerConfig: router,
     );
   }
