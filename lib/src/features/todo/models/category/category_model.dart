46c7d60eeacb56f10a22ef4679cff95ec374311a
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-09-19 00:21:12 +0100

    Refactor Category Model to remove helper functions

--- lib/src/features/todo/models/category/category_model.dart
+++ lib/src/features/todo/models/category/category_model.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-//DONE
+import 'package:uuid/uuid.dart';
 
 /// -- Data class to model the [Category] with name, icon, iconColor and
 ///  backgroundColor generate by changing the iconColor
 /// Required:
+///   - [id] - Generate Category id
 ///   - [name] - Category name
 ///   - [icon] - Category icon
 ///   - [iconColor] - Category icon
@@ -12,7 +13,12 @@ import 'package:flutter/material.dart';
 /// Generate:
 ///   - [backgroundColor] - Generate by converting the [iconColor] to HSL and
 /// modifying it's Lightness value to [75]
+///
+
+const uuid = Uuid();
+
 class Category {
+  final String id;
   String name;
   bool isCreateNewButton;
   IconData icon;
@@ -20,33 +26,12 @@ class Category {
   Color backgroundColor;
 
   Category({
+    id,
     required this.name,
     this.isCreateNewButton = false,
     required this.icon,
     required this.iconColor,
-  }) : backgroundColor =
+  })  : id = id ?? uuid.v1(),
+        backgroundColor =
             HSLColor.fromColor(iconColor).withLightness(0.75).toColor();
-
-  // Convert Category to JSON
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'isCreateNewButton': isCreateNewButton,
-      'icon': icon.codePoint, // Store icon as its code point
-      'iconColor': iconColor.value, // Store color as an integer value
-      'backgroundColor':
-          backgroundColor.value, // Store background color as an integer
-    };
-  }
-
-  /// Create a Category from JSON
-  factory Category.fromJson(Map<String, dynamic> json) {
-    return Category(
-      name: json['name'],
-      isCreateNewButton: json['isCreateNewButton'] ?? false,
-      icon: IconData(json['icon'],
-          fontFamily: 'MaterialIcons'), // Restore IconData using code point
-      iconColor: Color(json['iconColor']), // Restore Color using integer value
-    );
-  }
 }
