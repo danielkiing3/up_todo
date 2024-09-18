4be8fc981e37238b39cc19882b45b03c44a6c138
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-09-19 00:21:30 +0100

    Update Category Adapter

--- lib/src/features/todo/models/category/hive_adapter/category_adapter.dart
+++ lib/src/features/todo/models/category/hive_adapter/category_adapter.dart
@@ -7,7 +7,7 @@ const iconFamily = 'IconsaxPlusLinear';
 /// Custom Adapter for the [Category] model used by Hive
 class CategoryAdapter extends TypeAdapter<Category> {
   @override
-  final typeId = 3;
+  int get typeId => 2;
 
   @override
   Category read(BinaryReader reader) {
@@ -15,12 +15,15 @@ class CategoryAdapter extends TypeAdapter<Category> {
     final isCreatedNewButton = reader.readBool(); // Explicitly reading a bool
     final iconCode = reader.readInt(); // Reading int for IconData's codePoint
     final iconColorValue = reader.readInt(); // Reading int for Color value
+    final id = reader.readString();
 
     return Category(
-        name: name,
-        icon: IconData(iconCode, fontFamily: iconFamily),
-        iconColor: Color(iconColorValue),
-        isCreateNewButton: isCreatedNewButton);
+      name: name,
+      icon: IconData(iconCode, fontFamily: iconFamily),
+      iconColor: Color(iconColorValue),
+      isCreateNewButton: isCreatedNewButton,
+      id: id,
+    );
   }
 
   @override
@@ -29,6 +32,7 @@ class CategoryAdapter extends TypeAdapter<Category> {
       ..writeString(obj.name) // Write name as a String
       ..writeBool(obj.isCreateNewButton) // Write isCreateNewButton as a bool
       ..writeInt(obj.icon.codePoint) // Write icon codePoint as an int
-      ..writeInt(obj.iconColor.value); // Write iconColor as an int
+      ..writeInt(obj.iconColor.value)
+      ..writeString(obj.id); // Write iconColor as an int
   }
 }
