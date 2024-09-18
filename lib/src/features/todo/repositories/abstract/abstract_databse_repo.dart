2021b24b3cc034b2efa7e6fa64318408732c9abc
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-09-19 00:23:20 +0100

    Update Parameters

--- lib/src/features/todo/repositories/abstract/abstract_databse_repo.dart
+++ lib/src/features/todo/repositories/abstract/abstract_databse_repo.dart
@@ -1,9 +1,9 @@
-import 'package:up_todo/src/features/todo/models/todo/todo_model.dart';
+import 'package:up_todo/src/features/todo/models/todo/task_model.dart';
 
 /// -- Abstract class for CRUD operation on Todo database
 abstract class DatabaseRepository {
-  Future<void> addNewTask(Todo todo); // Create
-  Future<List<Todo>> fetchAllTask(); // Read
-  Future<void> updateTask(int index, Todo todo); // Update
+  Future<void> addNewTask(Task todo); // Create
+  Future<List<Task>> fetchAllTask(); // Read
+  Future<void> updateTask(int index, Task todo); // Update
   Future<void> deleteTask(String id); // Delete
 }
