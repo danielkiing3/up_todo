487278343a3d9cfdc453881f131370293702fd3f
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-09-18 23:32:20 +0100

    Wrote Custom Adapter for Category Model

---
+++ lib/src/features/todo/models/category/category_adapter.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:up_todo/src/features/todo/models/category/category_model.dart';
+
+const iconFamily = 'IconsaxPlusLinear';
+
+/// Custom Adapter for the [Category] model used by Hive
+class CategoryAdapter extends TypeAdapter<Category> {
+  @override
+  final typeId = 3;
+
+  @override
+  Category read(BinaryReader reader) {
+    final name = reader.readString(); // Explicitly reading a String
+    final isCreatedNewButton = reader.readBool(); // Explicitly reading a bool
+    final iconCode = reader.readInt(); // Reading int for IconData's codePoint
+    final iconColorValue = reader.readInt(); // Reading int for Color value
+
+    return Category(
+        name: name,
+        icon: IconData(iconCode, fontFamily: iconFamily),
+        iconColor: Color(iconColorValue),
+        isCreateNewButton: isCreatedNewButton);
+  }
+
+  @override
+  void write(BinaryWriter writer, Category obj) {
+    writer
+      ..writeString(obj.name) // Write name as a String
+      ..writeBool(obj.isCreateNewButton) // Write isCreateNewButton as a bool
+      ..writeInt(obj.icon.codePoint) // Write icon codePoint as an int
+      ..writeInt(obj.iconColor.value); // Write iconColor as an int
+  }
+}
