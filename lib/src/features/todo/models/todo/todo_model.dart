9452d82c2023f089947b6a490c4f65d6713729fd
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-09-19 00:21:47 +0100

    Remove Hive Annotation

--- lib/src/features/todo/models/todo/todo_model.dart
+++ lib/src/features/todo/models/todo/todo_model.dart
@@ -1,69 +1,90 @@
-import '../category/category_model.dart';
+import 'package:uuid/uuid.dart';
 
-class Todo {
+const uuid = Uuid();
+
+class Task {
   final String id;
   final String title;
   final String? description;
   final DateTime? date; // Might change to datetime type
   final String? time;
   final int? priority;
-  final Category? category; // Type should be Category model class
+  final String? categoryId; // Type should be Category model class
+  final bool isCompleted;
 
-  Todo({
-    // Generate Todo id
-    this.id = '',
+  Task({
+    id,
     required this.title,
     this.description,
     this.date, // Initialize to default been today's date
     this.time,
     this.priority,
-    this.category,
-  });
+    this.categoryId,
+    this.isCompleted = false,
+  }) : id = id ?? uuid.v1();
 
-  /// Convert the [Todo] to a JSON object for storing in Firebase
+  /// Convert the [Task] to a JSON object for storing in Firebase
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'Title': title,
       'Description': description,
-      'Date': date,
+      'Date': date?.toIso8601String(),
       'Time': time,
       'Priority': priority,
-      'Category': category,
+      'Category': categoryId,
+      'isCompleted': isCompleted,
     };
   }
 
-  factory Todo.fromMap(Map<String, dynamic> json) {
-    return Todo(
-      id: json['id'] ?? '',
-      title: json['Title'] ?? '',
-      description: json['Description'] ?? '',
-      date: json['Date'] ?? '',
-      time: json['Time'] ?? '',
-      priority: json['Priority'] ?? '',
-      category: json['Category'] ?? '',
+  factory Task.fromMap(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'] as String,
+      title: json['Title'] as String,
+      description: json['Description'],
+      date: json['Date'],
+      time: json['Time'],
+      priority: json['Priority'],
+      categoryId: json['Category'],
+      isCompleted: json['isCompleted'] as bool,
     );
   }
 }
 
 // Helper extension used to update values of the todo
 
-extension MutableTodo on Todo {
-  Todo update({
+extension MutableTodo on Task {
+  /// -- Function to update the values of a todo and return a new todo with same id
+  Task update({
     String? title,
     String? description,
     DateTime? date,
     String? time,
     int? priority,
-    Category? category,
+    String? categoryId,
   }) {
-    return Todo(
+    return Task(
       id: id,
       title: title ?? this.title,
       description: description ?? this.description,
       date: date ?? this.date,
       time: time ?? this.time,
       priority: priority ?? this.priority,
-      category: category ?? this.category,
+      categoryId: categoryId ?? this.categoryId,
+    );
+  }
+
+  /// -- Function to update the state of the Todo on completion state
+  Task complete(bool isCompleted) {
+    return Task(
+      id: id,
+      title: title,
+      description: description,
+      date: date,
+      time: time,
+      priority: priority,
+      categoryId: categoryId,
+      isCompleted: isCompleted,
     );
   }
 }
