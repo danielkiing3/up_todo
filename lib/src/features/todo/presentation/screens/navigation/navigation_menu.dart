f3f44b5cc815e889ed49f24b349935d30c0a018e
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-09-20 11:57:05 +0100

    Refactor to use Riverpod

--- lib/src/features/todo/presentation/screens/navigation/navigation_menu.dart
+++ lib/src/features/todo/presentation/screens/navigation/navigation_menu.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../../personalization/screens/profile/profile.dart';
 import '../../controllers/navigation/navigation_controller.dart';
+import '../calender/calender.dart';
+import '../focus/focus.dart';
+import '../index/index.dart';
 import 'widgets/bottom_nav_bar.dart';
 import 'widgets/nav_floating_action_button.dart';
 
 /// -- App Main Navigation Menu
-class NavigationMenu extends StatelessWidget {
+class NavigationMenu extends ConsumerWidget {
   const NavigationMenu({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final controller = Get.put(
-        NavigationMenuController()); // Initialization of the Navigation controller
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pageIndex = ref.watch(bottomNavigationProvider);
 
     return Scaffold(
       resizeToAvoidBottomInset: false, // fluter 2.x
@@ -26,7 +29,12 @@ class NavigationMenu extends StatelessWidget {
       bottomNavigationBar: const UBottomNavBar(),
 
       // Body that reactive to the [controller.currentPageIndex] to update state
-      body: Obx(() => controller.screen[controller.currentPageIndex.value]),
+      body: [
+        const IndexScreen(),
+        const CalenderScreen(),
+        const FocusScreen(),
+        const ProfileScreen(),
+      ][pageIndex],
     );
   }
 }
