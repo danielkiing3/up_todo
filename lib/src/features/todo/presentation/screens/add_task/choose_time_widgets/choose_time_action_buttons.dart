68d60dc3c9eb98c7e8282fbdff7e3c967485f257
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-09-24 23:35:22 +0100

    Build Choose TIme WIdgets

---
+++ lib/src/features/todo/presentation/screens/add_task/choose_time_widgets/choose_time_action_buttons.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../../../utils/constants/texts.dart';
+import '../../../controllers/add_task/choose_date_provider.dart';
+
+class ChooseTimeActionButtons extends ConsumerWidget {
+  const ChooseTimeActionButtons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Row(
+      children: [
+        // -- Cancel
+        Expanded(
+          child: TextButton(
+            onPressed: () =>
+                ref.read(chooseDateProvider.notifier).cancel(context),
+            child: const Text(UTexts.cancel),
+          ),
+        ),
+
+        // -- Save
+        Expanded(
+          child: ElevatedButton(
+            onPressed: () =>
+                ref.read(chooseDateProvider.notifier).save(context),
+            child: const Text(UTexts.save),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/src/features/todo/presentation/screens/add_task/choose_time_widgets/choose_time_container.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:up_todo/src/features/todo/presentation/controllers/add_task/choose_time_provider.dart';
+import 'package:up_todo/src/utils/constants/colors.dart';
+import 'package:up_todo/src/utils/constants/sizes.dart';
+
+class ChooseTimeContainer extends ConsumerWidget {
+  const ChooseTimeContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        // -- Hour
+        Container(
+          decoration: BoxDecoration(
+              color: UColors.darkestGrey,
+              borderRadius: BorderRadius.circular(6)),
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 90, maxWidth: 80),
+            child: ListWheelScrollView.useDelegate(
+              itemExtent: 30,
+              useMagnifier: true,
+              magnification: 1.5,
+              overAndUnderCenterOpacity: 0.2,
+              controller: ref.read(chooseTimeProvider.notifier).hourController,
+              childDelegate: ListWheelChildListDelegate(
+                children: Iterable<int>.generate(12)
+                    .toList()
+                    .map((hour) => Text((hour).toString()))
+                    .toList(),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: USizes.md),
+
+        // Dot
+        Text(':', style: Theme.of(context).textTheme.headlineLarge),
+        const SizedBox(width: USizes.md),
+
+        // -- Minute
+        Container(
+          decoration: BoxDecoration(
+              color: UColors.darkestGrey,
+              borderRadius: BorderRadius.circular(USizes.sm)),
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 90, maxWidth: 80),
+            child: ListWheelScrollView.useDelegate(
+              itemExtent: 30,
+              useMagnifier: true,
+              magnification: 1.5,
+              controller:
+                  ref.read(chooseTimeProvider.notifier).minuteController,
+              overAndUnderCenterOpacity: 0.2,
+              childDelegate: ListWheelChildListDelegate(
+                children: Iterable<int>.generate(60)
+                    .toList()
+                    .map((hour) => Text((hour).toString()))
+                    .toList(),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: USizes.md),
+
+        // -- AM / PM
+        Container(
+          decoration: BoxDecoration(
+              color: UColors.darkestGrey,
+              borderRadius: BorderRadius.circular(USizes.sm)),
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 90, maxWidth: 80),
+            child: ListWheelScrollView(
+              controller: ref.read(chooseTimeProvider.notifier).amPmController,
+              itemExtent: 30,
+              useMagnifier: true,
+              magnification: 1.5,
+              overAndUnderCenterOpacity: 0.2,
+              children: [
+                'AM',
+                "PM",
+              ].map((time) => Text(time)).toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
